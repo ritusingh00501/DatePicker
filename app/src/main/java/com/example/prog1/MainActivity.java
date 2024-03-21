d9c16f288b365eee4414c9c611dd4d2894ef22a5
ritusingh00501 <ritusingh00501@gmail.com>
2024-03-21 20:31:33 +0530

    Final Commit

---
+++ app/src/main/java/com/example/prog1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.prog1;
+//import android.support.v4.app.AppCompactActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    DatePicker picker;
+    Button btnGet;
+    TextView tvw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvw=(TextView)findViewById(R.id.textView1);
+        picker=(DatePicker)findViewById(R.id.datePicker1);
+        btnGet=(Button)findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: "+ picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear());
+            }
+        });
+    }
+}
